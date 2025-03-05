05f67e00f1b2b1e7eea3aa7b15afdb10bc040b70
Abraham <ighaloabraham2018@gmail.com>
2025-03-06 00:45:55 +0700

    fixed settings

--- alke/settings.py
+++ alke/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-n99xi2d09ci05+wwkxinqy+@9j93oj5bn92sna@!n3^o02x9nz"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
